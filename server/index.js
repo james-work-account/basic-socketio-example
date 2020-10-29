dca00e0860b63fc7f65e60b9826fa891063ea3a6
james-work-account <31282758+james-work-account@users.noreply.github.com>
2020-10-29 15:13:27 +0000

    escape HTML inputs

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
+const escape = require("escape-html");
+
 const app = express();
 
 app.use(express.static("website"));
@@ -13,8 +15,9 @@ const io = socketIo(server);
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  socket.on("new message from client", (data) => {
-    io.emit("server received a message", data);
+  socket.on("new message from client", ({ user, text }) => {
+    const escapedText = escape(text);
+    io.emit("server received a message", { user, text: escapedText });
   });
 });
 
--- website/index.js
+++ website/index.js
@@ -4,16 +4,15 @@ document.querySelector("form").addEventListener("submit", (e) => {
   // Stop the form from refreshing the page
   e.preventDefault();
 
-  const message = JSON.stringify({
+  const message = {
     user: document.getElementById("name").value,
     text: document.getElementById("formBody").value,
-  });
+  };
 
   socket.emit("new message from client", message);
 });
 
-socket.on("server received a message", (string) => {
-  const data = JSON.parse(string);
+socket.on("server received a message", (data) => {
   document.getElementById(
     "output"
   ).innerHTML += `<div class="border-2 border-gray-500 rounded p-3 my-3"><p class="font-bold">${data.user}</p><p class="font-light">${data.text}</p></div>`;
